2823366993e041da11cb2ab9548f31db07d10780
kayla <111674229+Spinchies@users.noreply.github.com>
2024-07-15 04:31:19 -0500

    Minor Update
    ~Fixed an issue that made it impossible to move the window
    
    ~Moved the buttons and added a separator bar at the top.

--- ModifySpotify/Form1.Designer.cs
+++ ModifySpotify/Form1.Designer.cs
@@ -29,121 +29,136 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            button1 = new Button();
-            button2 = new Button();
+            airForm1 = new ReaLTaiizor.Forms.AirForm();
             pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            button3 = new Button();
-            button4 = new Button();
+            foreverButton2 = new ReaLTaiizor.Controls.ForeverButton();
+            foreverButton1 = new ReaLTaiizor.Controls.ForeverButton();
+            hopePictureBox1 = new ReaLTaiizor.Controls.HopePictureBox();
+            airSeparator1 = new ReaLTaiizor.Controls.AirSeparator();
+            airForm1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)hopePictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // airForm1
             // 
-            button1.BackColor = Color.Lime;
-            button1.Cursor = Cursors.Hand;
-            button1.FlatAppearance.BorderColor = Color.Black;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.Location = new Point(181, 263);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 41);
-            button1.TabIndex = 0;
-            button1.Text = "Install Spicetify";
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.Lime;
-            button2.Cursor = Cursors.Hand;
-            button2.FlatAppearance.BorderColor = Color.Black;
-            button2.FlatStyle = FlatStyle.Flat;
-            button2.Location = new Point(100, 263);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 41);
-            button2.TabIndex = 1;
-            button2.Text = "Install SpotX";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
+            airForm1.BackColor = Color.FromArgb(64, 64, 64);
+            airForm1.BorderStyle = FormBorderStyle.None;
+            airForm1.Controls.Add(pictureBox1);
+            airForm1.Controls.Add(foreverButton2);
+            airForm1.Controls.Add(foreverButton1);
+            airForm1.Controls.Add(hopePictureBox1);
+            airForm1.Controls.Add(airSeparator1);
+            airForm1.Customization = "AAAA/1paWv9ycnL/";
+            airForm1.Dock = DockStyle.Fill;
+            airForm1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            airForm1.Image = null;
+            airForm1.Location = new Point(0, 0);
+            airForm1.MinimumSize = new Size(112, 35);
+            airForm1.Movable = true;
+            airForm1.Name = "airForm1";
+            airForm1.NoRounding = false;
+            airForm1.Sizable = false;
+            airForm1.Size = new Size(406, 406);
+            airForm1.SmartBounds = true;
+            airForm1.StartPosition = FormStartPosition.CenterScreen;
+            airForm1.TabIndex = 0;
+            airForm1.TransparencyKey = Color.Fuchsia;
+            airForm1.Transparent = false;
+            airForm1.Click += airForm1_Click;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(88, 94);
+            pictureBox1.Location = new Point(45, 54);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(178, 163);
-            pictureBox1.TabIndex = 2;
+            pictureBox1.Size = new Size(321, 46);
+            pictureBox1.TabIndex = 4;
             pictureBox1.TabStop = false;
             // 
-            // pictureBox2
+            // foreverButton2
             // 
-            pictureBox2.BackgroundImageLayout = ImageLayout.Center;
-            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(18, 46);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(320, 42);
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
+            foreverButton2.BackColor = Color.Transparent;
+            foreverButton2.BaseColor = Color.Cyan;
+            foreverButton2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            foreverButton2.Location = new Point(151, 328);
+            foreverButton2.Name = "foreverButton2";
+            foreverButton2.Rounded = false;
+            foreverButton2.Size = new Size(120, 40);
+            foreverButton2.TabIndex = 3;
+            foreverButton2.Text = "Install Spicetify";
+            foreverButton2.TextColor = Color.Black;
+            foreverButton2.Click += foreverButton2_Click;
             // 
-            // button3
+            // foreverButton1
             // 
-            button3.BackColor = Color.Red;
-            button3.Cursor = Cursors.Hand;
-            button3.FlatStyle = FlatStyle.Flat;
-            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.Location = new Point(319, -1);
-            button3.Name = "button3";
-            button3.Size = new Size(32, 31);
-            button3.TabIndex = 4;
-            button3.Text = "X";
-            button3.UseVisualStyleBackColor = false;
-            button3.Click += button3_Click;
+            foreverButton1.BackColor = Color.Transparent;
+            foreverButton1.BaseColor = Color.Lime;
+            foreverButton1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            foreverButton1.Location = new Point(151, 272);
+            foreverButton1.Name = "foreverButton1";
+            foreverButton1.Rounded = false;
+            foreverButton1.Size = new Size(120, 40);
+            foreverButton1.TabIndex = 2;
+            foreverButton1.Text = "Install SpotX";
+            foreverButton1.TextColor = Color.Black;
+            foreverButton1.Click += foreverButton1_Click;
             // 
-            // button4
+            // hopePictureBox1
             // 
-            button4.BackColor = Color.Cyan;
-            button4.FlatStyle = FlatStyle.Flat;
-            button4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button4.Location = new Point(290, -1);
-            button4.Name = "button4";
-            button4.Size = new Size(32, 31);
-            button4.TabIndex = 7;
-            button4.Text = "?";
-            button4.UseVisualStyleBackColor = false;
-            button4.Click += button4_Click_1;
+            hopePictureBox1.BackColor = Color.FromArgb(192, 196, 204);
+            hopePictureBox1.Image = (Image)resources.GetObject("hopePictureBox1.Image");
+            hopePictureBox1.Location = new Point(116, 106);
+            hopePictureBox1.Name = "hopePictureBox1";
+            hopePictureBox1.PixelOffsetType = System.Drawing.Drawing2D.PixelOffsetMode.HighQuality;
+            hopePictureBox1.Size = new Size(170, 160);
+            hopePictureBox1.SmoothingType = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            hopePictureBox1.TabIndex = 1;
+            hopePictureBox1.TabStop = false;
+            hopePictureBox1.TextRenderingType = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            hopePictureBox1.Click += hopePictureBox1_Click;
+            // 
+            // airSeparator1
+            // 
+            airSeparator1.BackColor = Color.Cyan;
+            airSeparator1.Customization = "";
+            airSeparator1.Font = new Font("Verdana", 8F, FontStyle.Regular, GraphicsUnit.Point);
+            airSeparator1.Image = null;
+            airSeparator1.Location = new Point(0, 33);
+            airSeparator1.Name = "airSeparator1";
+            airSeparator1.NoRounding = false;
+            airSeparator1.Size = new Size(429, 1);
+            airSeparator1.TabIndex = 0;
+            airSeparator1.Text = "airSeparator1";
+            airSeparator1.Transparent = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(64, 64, 64);
-            ClientSize = new Size(350, 379);
-            ControlBox = false;
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(406, 406);
+            Controls.Add(airForm1);
             FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1920, 1050);
+            MinimumSize = new Size(261, 65);
             Name = "Form1";
-            SizeGripStyle = SizeGripStyle.Show;
+            ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Form1";
-            Load += Form1_Load;
+            Text = "dungeonForm1";
+            TransparencyKey = Color.Fuchsia;
+            airForm1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)hopePictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private ReaLTaiizor.Forms.AirForm airForm1;
+        private ReaLTaiizor.Controls.AirSeparator airSeparator1;
+        private ReaLTaiizor.Controls.HopePictureBox hopePictureBox1;
+        private ReaLTaiizor.Controls.ForeverButton foreverButton2;
+        private ReaLTaiizor.Controls.ForeverButton foreverButton1;
         private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
-        private Button button3;
-        private Button button4;
     }
 }
--- ModifySpotify/Form1.cs
+++ ModifySpotify/Form1.cs
@@ -10,52 +10,46 @@ namespace ModifySpotify
             InitializeComponent();
         }
 
+        private void airForm1_Click(object sender, EventArgs e)
+        {
+
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
-            Directory.SetCurrentDirectory(Application.StartupPath);
-            Directory.CreateDirectory(Application.StartupPath + "/resources");
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void hopePictureBox1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("If you want to install the marketplace, Press Y when prompted");
-            Process.Start("powerShell.exe", "iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex ");
+            // "Easter Egg"
+            Process.Start("explorer","https://github.com/Spinchies/ModifySpotify/blob/main/README.md");
+
         }
 
-        private async void button2_Click(object sender, EventArgs e)
+        private async void foreverButton1_Click(object sender, EventArgs e)
         {
-
-            await Download(); 
+            // SpotX
+            await Download();
             Process.Start("auto_install.bat");
+        }
 
+        private void foreverButton2_Click(object sender, EventArgs e)
+        {
+            // Spicetify 
+            MessageBox.Show("If you want to install the theme & plugin marketplace, Press Y when prompted");
+            Process.Start("powerShell.exe", "iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex ");
         }
         async Task Download()
         {
             using (WebClient client = new WebClient())
             {
+                Directory.SetCurrentDirectory(Application.StartupPath);
+                Directory.CreateDirectory("resources");
                 string dir = Application.StartupPath + "/resources" + "/auto_install.bat";
                 client.DownloadFileAsync(new Uri("https://raw.githack.com/amd64fox/SpotX/main/scripts/Install_Auto.bat"), dir);
                 Directory.SetCurrentDirectory(Application.StartupPath + "/resources");
-                Thread.Sleep(350);
+                Thread.Sleep(375);
             }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            //Close Button aka X
-            Application.Exit();
-        }
-
-        private void button4_Click_1(object sender, EventArgs e)
-        {
-            // Help Button
-            var uri = "https://github.com/Spinchies/ModifySpotify/blob/main/README.md";
-            var psi = new System.Diagnostics.ProcessStartInfo
-            {
-                UseShellExecute = true,
-                FileName = uri
-            };
-            System.Diagnostics.Process.Start(psi);
         }
     }
 }
