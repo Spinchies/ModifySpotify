50e445f145101c04adf128a2c31a383f07c4af2d
kayla <111674229+Spinchies@users.noreply.github.com>
2025-10-20 01:34:29 -0500

    fixed app closing too early
    Changed up the UI a bit,
    Fixed Crash when installing SpotX

---
+++ Spodify/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Spodify
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SpringGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.SpringGreen;
+            this.button1.Location = new System.Drawing.Point(67, 249);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(397, 87);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Install SpotX";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(67, 344);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(397, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Install Spicetify";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(195, 453);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(151, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(-1, -2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(538, 578);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(537, 573);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ModifySpotify";
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.SystemColors.GrayText;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Spodify/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spodify
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        void spotterfy()
+        {
+            string url = "https://raw.githack.com/amd64fox/SpotX/main/scripts/Install_Auto.bat"; // Replace with your file URL
+            string downloadPath = Path.Combine(Path.GetTempPath(), "install.bat");
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFileCompleted += (s, ev) =>
+                {
+                    // Run as admin
+                    ProcessStartInfo psi = new ProcessStartInfo(downloadPath)
+                    {
+                        UseShellExecute = true,
+                        Verb = "runas"
+                    };
+                    try
+                    {
+                        Process.Start(psi);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Failed to run as admin: " + ex.Message);
+                    }
+                };
+                client.DownloadFileAsync(new Uri(url), downloadPath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            spotterfy();
+        }
+
+        // P/Invoke to set window foreground for SendKeys fallback
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string psCommand = "iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex";
+            string args = $"-NoProfile -ExecutionPolicy Bypass -Command \"{psCommand}\"";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = args,
+                UseShellExecute = false, // needed to redirect stdin
+                RedirectStandardInput = true,
+                // Optional: capture output if desired
+                RedirectStandardOutput = false,
+                RedirectStandardError = false,
+                CreateNoWindow = false, // try to show the window so users can see progress
+                WorkingDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+            };
+
+            try
+            {
+                Process proc = Process.Start(psi);
+                if (proc == null)
+                {
+                    MessageBox.Show("Failed to start PowerShell process.", "Start Failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Best-effort: write "Y" to redirected StandardInput (buffers if prompt occurs later)
+                try
+                {
+                    if (proc.StandardInput != null && !proc.StandardInput.BaseStream.CanWrite == false)
+                    {
+                        proc.StandardInput.WriteLine("Y");
+                        proc.StandardInput.Flush();
+                        // Do not necessarily close input immediately; closing might terminate interactive prompts unexpectedly.
+                    }
+                }
+                catch (Exception)
+                {
+                    // Swallow; we'll attempt SendKeys fallback below if necessary.
+                }
+
+                // Fallback: try to bring the console window to foreground and send keystrokes.
+                // Wait for MainWindowHandle to be available (timeout after ~3 seconds).
+                const int maxRetries = 30;
+                int retries = 0;
+                while (retries < maxRetries && proc.MainWindowHandle == IntPtr.Zero)
+                {
+                    Thread.Sleep(100); // 100ms * 30 = 3s
+                    proc.Refresh(); // refresh process info so MainWindowHandle may be updated
+                    retries++;
+                }
+
+                if (proc.MainWindowHandle != IntPtr.Zero)
+                {
+                    try
+                    {
+                        // Bring to foreground and send Y + Enter
+                        SetForegroundWindow(proc.MainWindowHandle);
+                        // Small delay to ensure window gets focus
+                        Thread.Sleep(100);
+                        SendKeys.SendWait("Y{ENTER}");
+                    }
+                    catch (Exception)
+                    {
+                        // Ignore SendKeys errors; process already received stdin write attempt.
+                    }
+                }
+            }
+            catch (Win32Exception win32Ex)
+            {
+                // PowerShell not found or blocked
+                MessageBox.Show("Failed to start PowerShell: " + win32Ex.Message, "Start Failed",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unexpected error: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", "https://qntm.org/camtime");
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
